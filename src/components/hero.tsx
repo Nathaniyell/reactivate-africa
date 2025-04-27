11aa78c40e7ca6e3cabdb9fe2aaa5ec0b867b685
Nathaniyell <essien.nathan@yahoo.com>
2025-04-27 18:30:51 +0100

    update hero section images

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -32,10 +32,10 @@ function useTypewriter(text: string, speed: number = 100) {
 
 export default function Hero() {
   const backgroundImages = [
-    '/raf_talk.jpg',
     '/raf1.jpg',
     '/raf2.jpg',
     '/raf_girls.jpg',
+    '/raf_talk.jpg',
   ];
 
   
